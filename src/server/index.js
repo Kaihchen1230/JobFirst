6353f288c6eecd6294dd6673b1af87eed73e7a83
jieJayLan <lanjie3458@gmail.com>
2019-03-22 21:41:17 -0400

    add sequelize library to db

--- src/server/index.js
+++ src/server/index.js
@@ -3,9 +3,30 @@ const os = require('os');
 var path = require("path");
 const publicPath = path.join(__dirname, '../..', 'public');
 const app = express();
-let name =2;
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+// credential
+// host     : '127.0.0.1',
+// user     : 'root',
+// password : '',
+// database : ',
+
+const Sequelize = require('sequelize');
+
+// Option 1: Passing parameters separately
+const sequelize = new Sequelize('WebRing', 'root', 'lanmao8888', {
+  host: '127.0.0.1',
+  dialect: 'mysql'
+});
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 app.get('*', (req, res) => {                       
     res.sendFile(path.join(publicPath, 'index.html'));                            
