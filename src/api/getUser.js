e5d99495c628d17bdd8b0ba92e74ed702be49b88
albert6051 <albert.chen6051@gmail.com>
2019-03-25 01:33:49 -0400

    Update getUser.js
    setup user model

--- src/api/getUser.js
+++ src/api/getUser.js
@@ -3,6 +3,22 @@ const Sequelize = require('sequelize');
 module.exports = (connection) => {
     return (req, res) => {
         // setup model for user
-        
+        const User =  sequelize.define('user', {
+            user_ID:{
+                type: Sequelize.INTEGER
+            },
+            user_name:{
+                type: Sequelize.STRING
+            },
+            user_number:{
+                type: Sequelize.STRING
+            },
+            user_email:{
+                type: Sequelize.STRING
+            },
+            user_address:{
+                type: Sequelize.STRING
+            },
+        })
     }
 }
