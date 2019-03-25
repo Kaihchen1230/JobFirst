fcb6c1195f389cda28a220cdf80625e97c6946c4
albert6051 <albert.chen6051@gmail.com>
2019-03-25 01:31:06 -0400

    Update getBusiness.js
    setup Business model

--- src/api/getBusiness.js
+++ src/api/getBusiness.js
@@ -1,7 +1,24 @@
 const Sequelize = require('sequelize');
 
-module.exports = (connection) => {
+module.exports = (sequelize) => {
     return (req, res) => {
         // setup model for business
+        const Business =  sequelize.define('business', {
+            business_ID:{
+                type: Sequelize.INTEGER
+            },
+            business_name:{
+                type: Sequelize.STRING
+            },
+            business_number:{
+                type: Sequelize.STRING
+            },
+            business_email:{
+                type: Sequelize.STRING
+            },
+            business_location:{
+                type: Sequelize.STRING
+            },
+        })
     }
 }
