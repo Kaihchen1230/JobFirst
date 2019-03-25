d7dd590223137e8aefb8fb4a5abff7200f76614a
albert6051 <albert.chen6051@gmail.com>
2019-03-25 01:38:09 -0400

    Update getJob.js
    setup job model

--- src/api/getJob.js
+++ src/api/getJob.js
@@ -3,6 +3,22 @@ const Sequelize = require('sequelize');
 module.exports = (connection) => {
     return (req, res) => {
         // setup model for job
-
+        const Job =  sequelize.define('job', {
+            job_ID:{
+                type: Sequelize.INTEGER
+            },
+            job_title:{
+                type: Sequelize.STRING
+            },
+            job_poster:{
+                type: Sequelize.INTEGER
+            },
+            job_description:{
+                type: Sequelize.STRING
+            },
+            user_requirement:{
+                type: Sequelize.STRING
+            },
+        })
     }
 }
