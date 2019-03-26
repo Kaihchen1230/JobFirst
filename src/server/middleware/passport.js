966ff3dbe859cffe4ba6cc61103b70e803917b65
Alan Lau <alanlau97@gmail.com>
2019-03-26 13:19:22 -0400

    signin

--- src/server/middleware/passport.js
+++ src/server/middleware/passport.js
@@ -53,4 +53,32 @@ module.exports = function (passport, credentials) {
         }
     ));
 
+    passport.use('local-signin', new LocalStrategy(
+        {
+            usernameField: 'email',
+            passwordField: 'password',
+            passReqToCallback: true
+        },
+        function (req, email, password, done) {
+            const Credentials = credentials;
+            const checkPassword = function (userpass, password) {
+                return bCrypt.compareSync(password, userpass);
+            }
+            Credentials.findOne({ where: { email: email } }).then((user => {
+                if (!user) {
+                    return done(null, false, {message: 'Email does not exist' });
+                }
+                if(!checkPassword(user.password, password)) {
+                    return done(null, false, { message: 'Incorrest password.' });
+                }
+                const userinfo = credentials.get();
+                return done(null, userinfo);
+            }).catch((err) => {
+                console.log("Error:", err);
+                return done(null, false, { message: 'Something went wrong with signin' });
+            }));
+        }
+
+    ));
+
 }
