87cf495e6d5c4141bb83d7c784fddadcac25afad
Siddharth Rajan <siddharthrajan10@gmail.com>
2019-03-26 22:26:37 -0400

    refactor and change style

--- src/components/login.js
+++ src/components/login.js
@@ -40,52 +40,49 @@ export default class Login extends Component {
   render() {
     return (
       <div className="Login">
-        <Card className="*">
-          <CardContent>
-            <h1>Already a User? Log in Below</h1>
-            <form className="*" noValidate autoComplete="off">
-            <TextField
-            id="standard-name"
-            label="Username"
-            className="*"
-            placeholder="Enter Email"
-            onChange="*"
-            margin="normal"
-          />
-          <br />
-          <TextField
-            id="standard-password-input"
-            label="Password"
-            className="*"
-            placeholder="Enter Password"
-            onChange="*"
-            type="password"
-            margin="normal"
-          />
-          <br />
-          <br />
-          <Button variant="contained" color="primary">
-            Submit
-          </Button>
-            
-            
-            </form>
-          </CardContent>
-        </Card>
-        <Card className="*">
-          <CardContent>
-            <h1>Sign Up As a New Business</h1>
-            <form className="*"></form>
-          </CardContent>
-        </Card>
-        <Card className="*">
-          <CardContent>
-            <h1>Sign Up As a New User</h1>
-            <form className="*"></form>
-          </CardContent>
-        </Card>
-        
-        
+        <header className="Login-header">
+          <Card className="*">
+            <CardContent>
+              <h1>Already a User? Log in Below</h1>
+              <form className="*" noValidate autoComplete="off">
+                <TextField
+                  id="standard-name"
+                  label="Username"
+                  className="*"
+                  placeholder="Enter Email"
+                  onChange="*"
+                  margin="normal"
+                />
+                <br />
+                <TextField
+                  id="standard-password-input"
+                  label="Password"
+                  className="*"
+                  placeholder="Enter Password"
+                  onChange="*"
+                  type="password"
+                  margin="normal"
+                />
+                <br />
+                <br />
+                <Button variant="contained" color="primary">Submit</Button>
+              </form>
+            </CardContent>
+          </Card>
+          <Card className="*">
+            <CardContent>
+              <h1>Sign Up As a New Business</h1>
+              <form className="*"></form>
+            </CardContent>
+          </Card>
+          <Card className="*">
+            <CardContent>
+              <h1>Sign Up As a New User</h1>
+              <form className="*"></form>
+            </CardContent>
+          </Card>
+
+        </header>
       </div>
     );
   }
