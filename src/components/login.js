deb4557fd2d3c5664f011a643e7f22d649eb8196
Siddharth Rajan <siddharthrajan10@gmail.com>
2019-03-26 21:48:31 -0400

    import necessary libraries

--- src/components/login.js
+++ src/components/login.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import '../style/app.scss';
+// import '../style/app.scss';
+// import './App.css';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 
 export default class Login extends Component {
   state = { username: null };
 
   componentDidMount() {
-    fetch('/api/getUsername')
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+//    fetch('/api/getUsername')
+//      .then(res => res.json())
+//      .then(user => this.setState({ username: user.username }));
   }
 
   render() {
