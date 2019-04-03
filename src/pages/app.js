e4af20a67d6c1d968d1405a3842c240f472f9c6f
Kaihchen1230 <kaihang.c@gmail.com>
2019-04-02 23:31:02 -0400

    finished the toturial

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,13 +1,14 @@
 import React from "react"
 import { Router } from "@reach/router"
 import Layout from "../components/layout"
+import PrivateRoute from "../components/privateRoute"
 import Profile from "../components/profile"
 import Login from "../components/login"
 
 const App = () => (
   <Layout>
     <Router>
-      <Profile path="/app/profile" />
+      <PrivateRoute path="/app/profile" component={Profile} />
       <Login path="/app/login" />
     </Router>
   </Layout>
