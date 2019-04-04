a22c90d4cf337eee05ea10ffe15118aee91bd3a4
Alan Lau <alanlau97@gmail.com>
2019-04-04 02:03:47 -0400

    user profile

--- src/components/resumeList.js
+++ src/components/resumeList.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import { List, Typography } from 'antd';
-
-const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-];
+import { List } from 'antd';
 
 const MyList = (props) => {
-    <div>
-        <h3 style={{ margin: '16px 0' }}>Large Size</h3>
-        <List
-            size="large"
-            header={<div>Header</div>}
-            footer={<div>Footer</div>}
-            bordered
-            dataSource={data}
-            renderItem={item => (<List.Item>{item}</List.Item>)}
-        />
-    </div>
+
+    const data = [
+        'Racing car sprays burning fuel into crowd.',
+        'Japanese princess to wed commoner.',
+        'Australian walks 100km after outback crash.',
+        'Man charged over missing wedding girl.',
+        'Los Angeles battles huge wildfires.',
+    ];
+    return (
+        <div>
+            <h3 style={{ margin: '16px 0' }}>Large Size</h3>
+            <List
+                size="large"
+                header={<div>Header</div>}
+                footer={<div>Footer</div>}
+                bordered
+                dataSource={data}
+                renderItem={item => (<List.Item>{item}</List.Item>)}
+            />
+        </div>
+    )
 }
 
 export default MyList;
--- src/pages/app.js
+++ src/pages/app.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Router } from "@reach/router"
 import Layout from "../components/layout"
 import PrivateRoute from "../components/privateRoute"
-import Welcome from "../components/welcome"
+//import Welcome from "../components/welcome"
 import Login from "../components/login"
 import postJob from "../components/postJob";
 import businessProfile from "../components/businessProfile"
@@ -12,7 +12,7 @@ import "antd/dist/antd.css";
 const App = () => (
   <Layout>
     <Router>
-      <PrivateRoute path="/app/welcome" component={Welcome} />
+      {/*<PrivateRoute path="/app/welcome" component={Welcome} /> */}
       <PrivateRoute path="/app/businessProfile" component={businessProfile} />
       <Login path="/app/login" />
       <PrivateRoute path="/app/postJob" component={postJob} />
