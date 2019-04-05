7155f779a088f249ea1329e21dc731767e17b02c
Alan Lau <alanlau97@gmail.com>
2019-04-05 14:49:11 -0400

    modified navbar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -2,47 +2,66 @@ import React from "react"
 import { Link, navigate } from "gatsby"
 import { getUser, isLoggedIn, logout } from "../services/auth"
 import { Auth } from "aws-amplify";
+import { Menu, Icon, Avatar } from 'antd';
 
-export default () => {
-  const content = { message: "", login: true }
-  if (isLoggedIn()) {
-    content.message = `Hello, ${getUser().username}`
-  } else {
-    content.message = "You are not logged in"
+const navBar = (props) => {
+  const state = {
+    login:
+      <span>
+        <Avatar shape="square" size="large" icon="user" />
+        <span> Log in </span>
+      </span>
   }
+  if (isLoggedIn()) {
+    state.login =
+        <span>
+          <Avatar shape="square" size="large" icon="user" />
+          <span>{getUser().username}</span>
+        </span>
+  } else {
+    state.login =
+      <span>
+        <Avatar shape="square" size="large" icon="user" />
+        <span> Log in </span>
+      </span>
+  }
+
   return (
-    <div
-      style={{
-        display: "flex",
-        flex: "1",
-        justifyContent: "space-between",
-        borderBottom: "1px solid #d1c1e0",
-      }}
+    <Menu
+      //onClick={this.handleClick}
+      //selectedKeys={[this.state.current]}
+      mode="horizontal"
+      theme="dark"
     >
-      <span>{content.message}</span> 
-      <nav>
-        <Link to="/">Home</Link>
-        {` `}
-        <Link to="/app/user-profile">Profile</Link> 
-        {` `}
-        <Link to="/app/business-profile">businessProfile</Link> 
-        {` `}
-        <Link to="/app/job-list">Job List</Link>
-        {` `}
-        {isLoggedIn() ? (
-          <a
-            href="/"
+      <Menu.Item key="home">
+        <Icon type="home" theme="twoTone" />Home
+        </Menu.Item>
+      <Menu.Item key="about">
+        <Icon type="profile" theme="twoTone" />About us
+        </Menu.Item>
+      <Menu.Item key="contact">
+        <Icon type="mail" theme="twoTone" />Contact us
+        </Menu.Item>
+      <Menu.Item key="register">
+        <Icon type="mail" theme="twoTone" />Register
+        </Menu.Item>
+      <Menu.Item key="login">
+        <Link to="/app/user-profile">{state.login}</Link>
+      </Menu.Item>
+      {isLoggedIn() ? (
+          <Menu.Item 
+            key="logout"
             onClick={event => {
-              event.preventDefault()
               Auth.signOut()
-              .then(logout(() => navigate(`/app/login`)))
-              .catch(err => console.log('error: ', err ))
-            }}
-          >
-            Logout
-          </a>
-        ) : null}
-      </nav>
-    </div>
-  )
+                .then(logout(() => navigate(`/app/login`)))
+                .catch(err => console.log('error: ', err))
+            }}>
+            <Icon type="close-circle" theme="twoTone" />Logout
+          </Menu.Item>
+      ) : null}
+
+    </Menu>
+  );
 }
+
+export default navBar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import { getUser, isLoggedIn } from "../services/auth"
-
 import Layout from "../components/layout"
-
 import Amplify from 'aws-amplify'
 import config from '../aws-exports'
 Amplify.configure(config)
