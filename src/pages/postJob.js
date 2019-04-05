7de265623131b25635cdc684d4bf8cb8bc397272
Siddharth Rajan <siddharthrajan10@gmail.com>
2019-04-04 22:35:13 -0400

    test new style

--- src/pages/postJob.js
+++ src/pages/postJob.js
@@ -2,44 +2,76 @@ import React from "react"
 //import { navigate } from "gatsby"
 //import { handleLogin, isLoggedIn } from "../services/auth"
 //import Layout from "../components/layout"
+import { Form, Icon, Input, Button } from 'antd';
 
 class postJob extends React.Component {
     render() {
+        const formItemLayout = {
+            labelCol: {
+                xs: { span: 24 },
+                sm: { span: 8 },
+            },
+            wrapperCol: {
+                xs: { span: 24 },
+                sm: { span: 16 },
+            },
+        };
+        const tailFormItemLayout = {
+            wrapperCol: {
+                xs: {
+                    span: 24,
+                    offset: 0,
+                },
+                sm: {
+                    span: 16,
+                    offset: 8,
+                },
+            },
+        };
         return (
             <div>
-            <h1>Post a New Job</h1>
-            <form
-                method="post"
-                onSubmit="*"
-            >
-                <label>Employer:
+                <h1>Post a New Job</h1>
+                <Form {...formItemLayout} onSubmit="*">
+                    <Form.Item
+                        label="E-mail"
+                    >
+                            <Input />
+                        
+                    </Form.Item>
+                </Form>
+
+                <form
+                    method="post"
+                    onSubmit="*"
+                >
+                    <label>Employer:
                     <input type="text" name="employer-name" onChange="*" />
-                </label>
-                <br />
-                <br />
-                <label>Date Posted:
+                    </label>
+                    <br />
+                    <br />
+                    <label>Date Posted:
                     <input type="date" name="job-post-date" />
-                </label>
-                <br />
-                <br />
-                <label>Job Type:
+                    </label>
+                    <br />
+                    <br />
+                    <label>Job Type:
                     <input type="text" name="job-type" />
-                </label>
-                <br />
-                <br />
-                <label>Job Description:
+                    </label>
+                    <br />
+                    <br />
+                    <label>Job Description:
                     <input type="text" name="job-description" />
-                </label>
-                <br />
-                <br />
-                <label>Contact Information:
+                    </label>
+                    <br />
+                    <br />
+                    <label>Contact Information:
                     <input type="text" name="job-contact" />
-                </label>
-                <br />
-                <br />
+                    </label>
+                    <br />
+                    <br />
                     <input type="submit" value="Add Job" />
-                <br />
-            </form>
+                    <br />
+                </form>
             </div>
         )
     }
