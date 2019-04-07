af8f302364d0bfcc09862eefd1948b2e90a0f88c
jieJayLan <lanjie3458@gmail.com>
2019-04-06 23:21:55 -0400

    add snapshot and description

--- src/components/snapshotTestExample/header.test.js
+++ src/components/snapshotTestExample/header.test.js
@@ -1,21 +1,12 @@
-// import React from "react"
-// import TestRenderer from 'react-test-renderer';
-
-// function MyComponent() {
-//   return (
-//     <div>
-//       <SubComponent foo="bar" />
-//       <p className="my">Hello</p>
-//     </div>
-//   )
-// }
-
 import React from 'react';
 import ReactShallowRenderer from 'react-test-renderer/shallow';
 import NavBar from "../navBar"
 
+
+describe("snapshot test",()=>{
 test('should render Header correctly', () => {
   const renderer = new ReactShallowRenderer();
   renderer.render(<NavBar />);
   expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
 });
--- src/testExample/sum.test.js
+++ src/testExample/sum.test.js
@@ -1,9 +1,13 @@
 const sum = require('./sum');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(4);
+describe("example test",()=>{
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
   });
+  
+  test('adds 1 + 2 to equal 3', () => {
+      expect(sum(1, 2)).toBe(4);
+    });
+})
+
