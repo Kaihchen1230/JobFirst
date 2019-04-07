52892df1aacd9fd1b8c670775e47539b8f3a9825
jieJayLan <lanjie3458@gmail.com>
2019-04-07 14:16:27 -0400

    change test snapshot

--- src/components/snapshotTestExample/navbar.test.js
+++ src/components/snapshotTestExample/navbar.test.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactShallowRenderer from 'react-test-renderer/shallow';
 import NavBar from "../navBar"
 
-
+//a snapshot test example
 describe("snapshot test",()=>{
-test('should render Header correctly', () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<NavBar />);
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
-});
+  test('should render Header correctly', () => {
+    const renderer = new ReactShallowRenderer();
+    renderer.render(<NavBar />);
+    expect(renderer.getRenderOutput()).toMatchSnapshot();
+  });
 });
--- src/unittestExample/sum.test.js
+++ src/unittestExample/sum.test.js
@@ -1,6 +1,5 @@
 const sum = require('./sum');
 
-
 describe("example test",()=>{
   test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
