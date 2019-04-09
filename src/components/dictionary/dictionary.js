8513e88e943ed874e8cea128729fbeae479ac243
jieJayLan <lanjie3458@gmail.com>
2019-04-08 23:23:53 -0400

    try to set up multiple language

---
+++ src/components/dictionary/dictionary.js
@@ -0,0 +1,11 @@
+let dict = {
+'fr': {
+    'Sign In': "Se connecter",
+    'Sign Up': "S'inscrire"
+},
+'es': {
+    'Sign In': "Registrarse",
+    'Sign Up': "Regístrate"
+}
+};
+export default dict;
