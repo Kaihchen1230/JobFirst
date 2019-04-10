6c254d0018763e7366ed9f11c50294fc28f2ed38
Siddharth Rajan <siddharthrajan10@gmail.com>
2019-04-09 21:05:24 -0400

    begin postJob test

---
+++ src/pages/snapshots/postJob.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactShallowRenderer from 'react-test-renderer/shallow';
+import PostJob from "../postJob"
+
+describe("postJob snapshot test", () => {
+    test('postJob render should match snapshot', () => {
+
+    });
+});
