218a9d6deefecfa39f0492ef0eddb0f06e75413c
Siddharth Rajan <siddharthrajan10@gmail.com>
2019-04-09 21:08:45 -0400

    begin postJob snapshot

--- src/pages/snapshots/postJob.test.js
+++ src/pages/snapshots/postJob.test.js
@@ -4,6 +4,8 @@ import PostJob from "../postJob"
 
 describe("postJob snapshot test", () => {
     test('postJob render should match snapshot', () => {
-
+        const render = new ReactShallowRenderer();
+        render.render(<PostJob />);
+        expect(render.getRenderOutput()).toMatchSnapshot();
     });
 });
