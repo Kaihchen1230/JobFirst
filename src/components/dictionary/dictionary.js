d8de5b10f2da1ef04a1ddfa0ae2bb6023e96a636
Siddharth Rajan <siddharthrajan10@gmail.com>
2019-04-11 23:26:36 -0400

    update dictionary

--- src/components/dictionary/dictionary.js
+++ src/components/dictionary/dictionary.js
@@ -4,14 +4,22 @@ let dict = {
         'Sign Up': "注册",
         'username': "用户名",
         'password': "密码",
-        'Post a New Job': "發布一份新工作"
+        'Post a New Job': "發布一份新工作",
+        'Enter Employer Name': "輸入雇主姓名",
+        'Date Posted On': "發布日期",
+        'Job Type': "工作類型",
+        'Enter Job Description': "輸入職位描述"
     },
     'es': {
         'Sign In': "Register",
         'Sign Up': "Register",
         'username': "username",
         'password': "password",
-        'Post a New Job': "Post a New Job"
+        'Post a New Job': "Post a New Job",
+        'Enter Employer Name': "Enter Employer Name",
+        'Date Posted On': "Date Posted On",
+        'Job Type': "Job Type",
+        'Enter Job Description': "Enter Job Description"
     }
 };
 export default dict;
