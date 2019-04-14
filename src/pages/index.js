2b8312f60e9b6907f2a6316e7d6753975e1cf296
Alan Lau <alanlau97@gmail.com>
2019-04-14 02:07:18 -0400

    added banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,11 @@ import { getUser, isLoggedIn } from "../services/auth"
 import Layout from "../components/layout"
 import Amplify from 'aws-amplify'
 import config from '../aws-exports'
+import Home from '../components/Home/home';
 Amplify.configure(config)
 
 export default () => (
   <Layout>
-    
+    <Home />
   </Layout>
 )
