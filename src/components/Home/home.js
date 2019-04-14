aafb5607bfc44fbe751955766af57c81ed872a91
Alan Lau <alanlau97@gmail.com>
2019-04-14 00:36:37 -0400

    init event

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -21,6 +21,72 @@ class Home extends React.Component {
             nextEnter: false,
             thumbEnter: false,
         };
+        [
+            'onChange', //
+            'prevEnter', // enter prev button
+            'prevLeave', // leave prev button
+            'nextEnter',
+            'nextLeave',
+            'onMouseEnter', // when mouse hoover
+            'onMouseLeave', // when mouse leave
+        ].forEach((method) => this[method] = this[method].bind(this));
     }
-    
+
+    onChange(type, int) {
+        if (type === 'before') {
+            this.setState({
+                intShow: int,
+            });
+        }
+    }
+
+    getNextPrevNumber() {
+        let nextInt = this.state.intShow + 1;
+        let prevInt = this.state.intShow - 1;
+        if (nextInt >= this.imgArray.length) {
+            nextInt = 0;
+        }
+        if (prevInt < 0) {
+            prevInt = this.imgArray.length - 1;
+        }
+
+        return [prevInt, nextInt];
+    }
+
+    prevEnter() {
+        this.setState({
+            prevEnter: true,
+        });
+    }
+
+    prevLeave() {
+        this.setState({
+            prevEnter: false,
+        });
+    }
+
+    nextEnter() {
+        this.setState({
+            nextEnter: true,
+        });
+    }
+
+    nextLeave() {
+        this.setState({
+            nextEnter: false,
+        });
+    }
+
+    onMouseEnter() {
+        this.setState({
+            thumbEnter: true,
+        });
+    }
+
+    onMouseLeave() {
+        this.setState({
+            thumbEnter: false,
+        });
+    }
+
 }
