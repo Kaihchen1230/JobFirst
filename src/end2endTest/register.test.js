ffae553670807f9e71e8709433a5d8029926ec93
Kaihchen1230 <kaihang.c@gmail.com>
2019-04-16 21:54:21 -0400

    completed e2e register and login test

--- src/end2endTest/register.test.js
+++ src/end2endTest/register.test.js
@@ -1,11 +1,34 @@
 const puppeteer = require('puppeteer');
 
-test('create an account in the website', async() => {
-    const browser = await puppeteer.launch({
-        headless: false
-    });
-    onst page = await browser.newPage();
-    await page.goto(
-      'https://master.d2eo52i5ucdlvi.amplifyapp.com/'
-    );
-});
+
+function delay(timeout) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeout);
+  });
+}
+test('should fill username', async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: ['--window-size=1920,1080']
+  });
+  const page = await browser.newPage();
+  await page.goto(
+    'http://localhost:8000/app/signup'
+  );
+  await page.click('[name="username"]');
+  await page.type('[name="username"]', 'testing2');
+  await page.click('[name="password"]');
+  await page.type('[name="password"]', 'testingPass1@');
+  await page.click('[name="email"]');
+  await page.type('[name="email"]','kaihang.c@gmail.com');
+  await page.click('[name="name"]');
+  await page.type('[name="name"]', 'testing name');
+  await page.click('[name="phone_number"]');
+  await page.type('[name="phone_number"]','+16467177149');
+  await page.click('label:nth-child(2)');
+  await page.click('#sign-up');
+  await page.waitFor(10000);
+  browser.close();
+  
+},1000000);
