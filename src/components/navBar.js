df49fcbe9a10a3c0366b9e7204aba5ae0380a25a
Siddharth Rajan <siddharthrajan10@gmail.com>
2019-04-23 20:09:18 -0400

    fix language button navbar issue

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -11,10 +11,8 @@ import dict from "./dictionary/dictionary";
 const navBar = (props) => {
   const state = {
     login:
-      <span>
-        {/* <Avatar shape="square" size="large" icon="user" /> */}
-        <NewLogin />
-      </span>,
+      /* <Avatar shape="square" size="large" icon="user" /> */
+      <NewLogin />
   };
   let lan = window.localStorage.getItem('lan')
   // the login button should change based on the user
@@ -51,75 +49,79 @@ const navBar = (props) => {
   )
 
   return (
-    <Menu
-      mode="horizontal"
-      theme="dark"
-      style={{ position: "sticky", top: "0", zIndex: 1 }}
-    >
-      <Menu.Item key="home" >
-        <Icon type="home" theme="outlined" style={{ style }} />
-        {I18n.get('Home')}
-        <Link to="/"></Link>
-      </Menu.Item>
-
-
-      <Menu.Item key="about">
-        <Icon type="solution" theme="outlined" />{I18n.get('View Job')}
-        <Link to="/app/job-list"></Link>
-      </Menu.Item>
-
-      <Menu.Item>
-        <Icon type="file-add" theme="outlined" />{I18n.get('Post a New Job')}
-        <Link to="/app/postJob"></Link>
-      </Menu.Item>
-
-      <Menu.Item>
-        <Icon type="bar-chart" theme="outlined" />{I18n.get('Business Profile')}
-        <Link to="/app/business-profile"></Link>
-      </Menu.Item>
-
-
-      <Menu.Item key="contact">
-        <Icon type="mail" theme="outlined" />{I18n.get('Contact Us')}
-      </Menu.Item>
-
-      <Dropdown overlay={language_menu}>
-        <Button>Language</Button>
-      </Dropdown>
-
-      {!isLoggedIn() ? (
-        <Menu.Item>
-          <Link to="/app/signup">
-            <Button type="primary">{I18n.get('Register')}</Button>
-          </Link>
+    <div align="center">
+      <Menu
+        mode="horizontal"
+        theme="dark"
+        style={{ position: "sticky", top: "0", zIndex: 1 }}
+      >
+        <Menu.Item key="home" >
+          <Icon type="home" theme="outlined" style={{ style }} />
+          {I18n.get('Home')}
+          <Link to="/"></Link>
         </Menu.Item>
-      ) : null
-      }
 
-      {isLoggedIn() ? (
-        <Menu.Item>
-          {state.login}
-          <Link to={`/app/user-profile/${getUser().sub}`}></Link>
+        <Menu.Item key="about">
+          <Icon type="solution" theme="outlined" />{I18n.get('View Job')}
+          <Link to="/app/job-list"></Link>
         </Menu.Item>
-      ) : (
+
+        <Menu.Item>
+          <Icon type="file-add" theme="outlined" />{I18n.get('Post a New Job')}
+          <Link to="/app/postJob"></Link>
+        </Menu.Item>
+
+        <Menu.Item>
+          <Icon type="bar-chart" theme="outlined" />{I18n.get('Business Profile')}
+          <Link to="/app/business-profile"></Link>
+        </Menu.Item>
+
+        <Menu.Item key="contact">
+          <Icon type="mail" theme="outlined" />{I18n.get('Contact Us')}
+        </Menu.Item>
+
+
+        {!isLoggedIn() ? (
+          <Menu.Item>
+            <Link to="/app/signup">
+              <Button type="primary">{I18n.get('Register')}</Button>
+            </Link>
+          </Menu.Item>
+        ) : null
+        }
+
+        {isLoggedIn() ? (
           <Menu.Item>
             {state.login}
+            <Link to={`/app/user-profile/${getUser().sub}`}></Link>
           </Menu.Item>
-        )}
-
-      {/* if logged in, then display log out button */}
-      {isLoggedIn() ? (
-        <Menu.Item
-          key="logout"
-          onClick={event => {
-            Auth.signOut()
-              .then(logout(() => navigate(`/`)))
-              .catch(err => console.log('error: ', err))
-          }}>
-          <Icon type="logout" theme="outlined" />Logout
+        ) : (
+            <Menu.Item>
+              {state.login}
             </Menu.Item>
-      ) : null}
-    </Menu>
+          )}
+
+        {/* if logged in, then display log out button */}
+        {isLoggedIn() ? (
+          <Menu.Item
+            key="logout"
+            onClick={event => {
+              Auth.signOut()
+                .then(logout(() => navigate(`/`)))
+                .catch(err => console.log('error: ', err))
+            }}>
+            <Icon type="logout" theme="outlined" />Logout
+        </Menu.Item>
+        ) : null}
+
+        <Menu.Item>
+          <Dropdown overlay={language_menu}>
+            <Button>Language</Button>
+          </Dropdown>
+        </Menu.Item>
+
+      </Menu>
+    </div>
   );
 }
 
