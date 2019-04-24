dcf504262eac71ceeef80328e0f7c83a301fd09d
Siddharth Rajan <siddharthrajan10@gmail.com>
2019-04-24 13:40:20 -0400

    begin work on jobs applied table

--- src/components/user_profile/content.js
+++ src/components/user_profile/content.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Tabs } from 'antd';
+import { Tabs, Table } from 'antd';
 import { getUser } from '../../services/auth';
 
 const TabPane = Tabs.TabPane;
@@ -7,6 +7,32 @@ const TabPane = Tabs.TabPane;
 function callback(key) {
     console.log(key);
 }
+
+const dataSource = [{
+    key: '1',
+    name: 'Mike',
+    age: 32,
+    address: '10 Downing Street'
+  }, {
+    key: '2',
+    name: 'John',
+    age: 42,
+    address: '10 Downing Street'
+  }];
+
+const columns = [{
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  }, {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  }, {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  }];
 
 const Information = (props) => {
     // should get all the user information from props
@@ -30,8 +56,12 @@ const Information = (props) => {
 
             <TabPane tab="Experience and Skills" key="3">Content of Tab Pane 3</TabPane>
             {(getUser().sub === user.id) ?
-                <TabPane tab="Jobs Applied" key="4">Content of Tab Pane 3</TabPane>:
-                null}
+                <TabPane tab="Jobs Applied" key="4">Content of Tab Pane 4
+                    <h1>Test</h1>
+                    <Table dataSource={dataSource} columns={columns} />
+                </TabPane>:
+                null
+            }
         </Tabs>
     );
 }
