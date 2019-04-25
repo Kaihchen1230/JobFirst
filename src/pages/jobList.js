0161ca9538cfb4446ba29db8f357ab18b4a37abb
albert6051 <albert.chen6051@gmail.com>
2019-04-25 04:07:46 -0400

    update job list page for search and filter display

--- src/pages/jobList.js
+++ src/pages/jobList.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import { generate } from 'randomstring';
-import { Layout } from 'antd';
+import { Col, Row, Select, Input, Icon, Menu, Layout } from 'antd';
 import JobItem from '../components/jobList/jobItem';
-import SideBar from '../components/jobList/sideBar';
-import { graphqlOperation } from 'aws-amplify';
+import dict from "../components/dictionary/dictionary";
+import { I18n, graphqlOperation } from 'aws-amplify';
 import * as queries from '../graphql/queries';
 import { Connect } from "aws-amplify-react";
 
 const {
     Header, Footer, Sider, Content,
 } = Layout;
-
+const Search = Input.Search;
+const SubMenu = Menu.SubMenu;
+const Option = Select.Option;
+const InputGroup = Input.Group;
+let lan = window.localStorage.getItem('lan');
+I18n.putVocabularies(dict);
+I18n.setLanguage(lan);
 class JobList extends React.Component {
-    state = {
+
+    state = {}
+
+    handleChange = (value) => {
+        console.log('select ${value}');
     }
 
     render() {
@@ -20,7 +29,57 @@ class JobList extends React.Component {
             <container>
                 <Layout>
                     <Header>
-                        <SideBar />
+                        <InputGroup compact>
+                            <Select size="large" defaultValue="Name" style={{ width: "10%" }}>
+                                <Option value="Name">Name</Option>
+                                <Option value="Location">Location</Option>
+                            </Select>
+                            <Search
+                                style={{ width: '54%' }}
+                                placeholder={I18n.get('Search')}
+                                enterButton="Search"
+                                size="large"
+                                onSearch={value => console.log(value)}
+                            />
+                        </InputGroup>
+                        <InputGroup compact>
+                            <Row gutter={8} style={{ width: '80%' }}>
+                                <Col span={4}>
+                                    <Select style={{ width: "80%" }} size="large" placeholder="Category">
+                                        <Option value="Option1-1">Option1-1</Option>
+                                        <Option value="Option1-2">Option1-2</Option>
+                                    </Select>
+                                </Col>
+                                <Col span={4}>
+                                    <Select style={{ width: "80%" }} size="large" placeholder="Type">
+                                        <Option value="Option1-1">{I18n.get('Full Time')}</Option>
+                                        <Option value="Option1-2">{I18n.get('Part Time')}</Option>
+                                        <Option value="Option1-3">{I18n.get('Internship')}</Option>
+                                        <Option value="Option1-4">{I18n.get('Temporary')}</Option>
+                                    </Select>
+                                </Col>
+                                <Col span={4}>
+                                    <Select style={{ width: "80%" }} size="large" placeholder="Education">
+                                        <Option value="Option1-1">Option1-1</Option>
+                                        <Option value="Option1-2">Option1-2</Option>
+                                    </Select>
+                                </Col>
+                                <Col span={4}>
+                                    <Select style={{ width: "80%" }} size="large" placeholder="Company Scale">
+                                        <Option value="Option1-1">Option1-1</Option>
+                                        <Option value="Option1-2">Option1-2</Option>
+                                    </Select>
+                                </Col>
+                                <Col span={4}>
+                                    <Select style={{ width: "80%" }} size="large" placeholder="Post Day">
+                                        <Option value="Option1-1">{I18n.get('15 Days')}</Option>
+                                        <Option value="Option1-2">{I18n.get('One Month')}</Option>
+                                        <Option value="Option1-3">{I18n.get('Three Months')}</Option>
+                                        <Option value="Option1-4">{I18n.get('All')}</Option>
+                                    </Select>
+                                </Col>
+                            </Row>
+                        </InputGroup>
                     </Header>
                 </Layout>
                 <Layout>
