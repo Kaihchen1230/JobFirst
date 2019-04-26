2c5596aa5a48b8e43c95306449dd99e8d1a93959
Kaihchen1230 <kaihang.c@gmail.com>
2019-04-25 20:08:25 -0400

    comment out lan

--- src/pages/jobList.js
+++ src/pages/jobList.js
@@ -14,8 +14,8 @@ const Search = Input.Search;
 const Option = Select.Option;
 const InputGroup = Input.Group;
 // let lan = window.localStorage.getItem('lan');
-I18n.putVocabularies(dict);
-I18n.setLanguage(lan);
+// I18n.putVocabularies(dict);
+// I18n.setLanguage(lan);
 class JobList extends React.Component {
 
     state = {
