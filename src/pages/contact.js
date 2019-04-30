0ef466066adc713ee4cd50785a4146da21fa3eec
Siddharth Rajan <siddharthrajan10@gmail.com>
2019-04-29 20:36:54 -0400

    add image to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { I18n } from "aws-amplify";
 import { Layout, Skeleton, Menu, Icon } from 'antd';
+import officeImage from "../../static/office.jpg";
 
 class Contact extends React.Component {
 
-render() {
-    return (
-        <div>
-            <h1>Contact Us</h1>
-            <br />
-            <p>+1800JOBFIRST</p>
-            <br />
-            <p>HeadQuarters: New York City</p>
-        </div>
-    );
-}
+    render() {
+        return (
+
+            <div style={{ backgroundImage: `url(${officeImage})`, height: "100%" }}>
+                <div>
+                    <br />
+                    <h1 align="center">Contact Us</h1>
+                    <br />
+                    <p>+1800JOBFIRST</p>
+                    <br />
+                    <p>HeadQuarters: New York City</p>
+                    <br />
+                </div>
+            </div>
+        );
+    }
 
 }
 
