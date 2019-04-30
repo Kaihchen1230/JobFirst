17c8d350337748ce5748baa0884b4aaa56e64932
Siddharth Rajan <siddharthrajan10@gmail.com>
2019-04-29 20:09:57 -0400

    add contact.js file

---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { I18n } from "aws-amplify";
+import { Layout, Skeleton, Menu, Icon } from 'antd';
+
+class Contact extends React.Component {
+
+render() {
+    return (
+
+    );
+}
+
+}
+
+export default Contact;
