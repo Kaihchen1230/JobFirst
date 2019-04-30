ca42cdc0375bd012dccc79b4596fe82632eb8f48
Siddharth Rajan <siddharthrajan10@gmail.com>
2019-04-29 20:20:46 -0400

    add very basic items in contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,7 +6,13 @@ class Contact extends React.Component {
 
 render() {
     return (
-
+        <div>
+            <h1>Contact Us</h1>
+            <br />
+            <p>+1800JOBFIRST</p>
+            <br />
+            <p>HeadQuarters: New York City</p>
+        </div>
     );
 }
 
