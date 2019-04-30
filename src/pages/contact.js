67a869c7cb94989e63539e3ab39e08f6256c5be2
Siddharth Rajan <siddharthrajan10@gmail.com>
2019-04-29 21:52:35 -0400

    use simple-flexbox to align cards

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,6 +3,7 @@ import { I18n } from "aws-amplify";
 import { Layout, Skeleton, Menu, Icon, Card } from 'antd';
 import officeImage from "../../static/office.jpg";
 import "../style/contact.css";
+import { Column, Row } from 'simple-flexbox';
 
 class Contact extends React.Component {
 
@@ -10,30 +11,34 @@ class Contact extends React.Component {
         return (
 
             <div align="center" className="mainDiv" style={{ backgroundImage: `url(${officeImage})` }}>
-                
+                <br />
                 <h1 className="title" align="center">Contact Us</h1>
                 <br />
                 <p className="description" align="center">You can get in touch with us at any time for 24/7 support and troubleshooting.</p>
-                <div className='rows'>
-                    <div className='row'>
-                        <Card
-                            size="default"
-                            title="Default size card"
-                        >
-                            <p>+1800JOBFIRST</p>
-                        </Card>
-                    </div>
-                    {" "}
-                    <div className='row'>
-                        <Card
-                            size="default"
-                            title="Default size card"
-                        >
-                            <p>HeadQuarters: New York City</p>
-                        </Card>
-                    </div>
-                </div>
-                
+                <Column flexGrow={1}>
+                    <Row horizontal='center'>
+                        <h1>HEADER</h1>
+                    </Row>
+                    <Row vertical='center'>
+                        <Column flexGrow={1} horizontal='center'>
+                            <Card
+                                size="default"
+                                title="Default size card"
+                            >
+                                <p>+1800JOBFIRST</p>
+                            </Card>
+                        </Column>
+                        <Column flexGrow={1} horizontal='center'>
+                            <Card
+                                size="default"
+                                title="Default size card"
+                            >
+                                <p>HeadQuarters: New York City</p>
+                            </Card>
+                        </Column>
+                    </Row>
+                </Column>
+
             </div>
         );
     }
