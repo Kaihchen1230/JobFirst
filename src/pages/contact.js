fd741fc4284ff3fbeed6385339aa65ae078ecb84
Siddharth Rajan <siddharthrajan10@gmail.com>
2019-04-29 20:54:09 -0400

    make basic structure of contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,22 +2,21 @@ import React from 'react';
 import { I18n } from "aws-amplify";
 import { Layout, Skeleton, Menu, Icon } from 'antd';
 import officeImage from "../../static/office.jpg";
+import "../style/contact.css";
 
 class Contact extends React.Component {
 
     render() {
         return (
 
-            <div style={{ backgroundImage: `url(${officeImage})`, height: "100%" }}>
-                <div>
-                    <br />
-                    <h1 align="center">Contact Us</h1>
-                    <br />
-                    <p>+1800JOBFIRST</p>
-                    <br />
-                    <p>HeadQuarters: New York City</p>
-                    <br />
-                </div>
+            <div style={{ backgroundImage: `url(${officeImage})` }}>
+                <br />
+                <h1 align="center">Contact Us</h1>
+                <br />
+                <p>+1800JOBFIRST</p>
+                <br />
+                <p>HeadQuarters: New York City</p>
+                <br />
             </div>
         );
     }
