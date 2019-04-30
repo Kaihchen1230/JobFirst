f8414274ea8b88ffc642bb16fdbe037dd2439b6e
Siddharth Rajan <siddharthrajan10@gmail.com>
2019-04-29 21:05:32 -0400

    begin adding card to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { I18n } from "aws-amplify";
-import { Layout, Skeleton, Menu, Icon } from 'antd';
+import { Layout, Skeleton, Menu, Icon, Card } from 'antd';
 import officeImage from "../../static/office.jpg";
 import "../style/contact.css";
 
@@ -11,8 +11,10 @@ class Contact extends React.Component {
 
             <div style={{ backgroundImage: `url(${officeImage})` }}>
                 <br />
-                <h1 align="center">Contact Us</h1>
+                <h1 className="title" align="center">Contact Us</h1>
                 <br />
+                <p className="description" align="center">You can get in touch with us at any time for 24/7 support and troubleshooting.</p>
+                <Card size="small" style={{ width: 300 }}></Card>
                 <p>+1800JOBFIRST</p>
                 <br />
                 <p>HeadQuarters: New York City</p>
