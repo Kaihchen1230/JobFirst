90275ce25d7e99ca2b73fbcc127e4cc5d80e760a
Siddharth Rajan <siddharthrajan10@gmail.com>
2019-04-30 20:54:03 -0400

    attempt e2e language test

--- src/end2endTest/language.test.js
+++ src/end2endTest/language.test.js
@@ -1,15 +1,22 @@
 const puppeteer = require('puppeteer');
 
+// function delay(timeout) {
+//     return new Promise((resolve) => {
+//       setTimeout(resolve, timeout);
+//     });
+//   }
 test('should change language to chinese', async () => {
     const browser = await puppeteer.launch({
         headless: false,
         slowMo: 80,
-        args: ['--window-size=1920,1080']
+        args: ['--window-size=2000,1080']
     });
     const page = await browser.newPage();
     await page.goto(
         'http://localhost:8000/'
     );
-    await page.click('Language');
-    await page.click('');
+    //await page.waitFor(1000);
+    await page.click('button#lanButton');
+    await browser.close();
+    //await page.click('CHINESE - 中文');
 })
