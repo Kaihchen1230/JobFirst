e5cdbb567d736627127d571164db4da6832177ea
albert6051 <albert.chen6051@gmail.com>
2019-05-02 04:54:09 -0400

    update toEqual from toBe

--- src/pages/jobListUtil.test.js
+++ src/pages/jobListUtil.test.js
@@ -2,20 +2,20 @@ const { filterTypeGen, filterDateGen, searchByNameGen, resetGen, } = require('./
 
 test('should give object for filter by job type', () => {
     const obj = filterTypeGen('full time');
-    expect(obj).toBe({ "filter": { "jobType": { "contains": 'full time' } } });
+    expect(obj).toEqual({ "filter": { "jobType": { "contains": 'full time' } } });
 });
 
 test('should give object for search by title', () => {
     const obj = searchByNameGen('computer', 'Name');
-    expect(obj).toBe({ "filter": { "searchFieldName": { "contains": 'computer' } } });
+    expect(obj).toEqual({ "filter": { "searchFieldName": { "contains": 'computer' } } });
 });
 
 test('should give object for search by location', () => {
     const obj = searchByNameGen('180', 'Location');
-    expect(obj).toBe({ "filter": { "searchFieldLocation": { "contains": '180' } } });
+    expect(obj).toEqual({ "filter": { "searchFieldLocation": { "contains": '180' } } });
 });
 
 test('should return an empty object', () => {
     const obj = resetGen();
-    expect(obj).toBe({});
+    expect(obj).toEqual({});
 });
