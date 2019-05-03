e94c98eab0510b0b88b7beac849624d0c515d595
Kaihchen1230 <kaihang.c@gmail.com>
2019-05-03 00:54:50 -0400

    comment out test

--- src/pages/jobListUtil.js
+++ src/pages/jobListUtil.js
@@ -1,30 +1,30 @@
-exports.filterTypeGen = (value) => {
-    let newSearch = {};
-    if(value == "All"){
-        return {"filter":{}};
-    }else{
-        newSearch = {"filter":{"jobType":{"contains":value}}};
-        return newSearch;
-    }
-}
+// exports.filterTypeGen = (value) => {
+//     let newSearch = {};
+//     if(value == "All"){
+//         return {"filter":{}};
+//     }else{
+//         newSearch = {"filter":{"jobType":{"contains":value}}};
+//         return newSearch;
+//     }
+// }
 
-exports.filterDateGen = (value) =>{
+// exports.filterDateGen = (value) =>{
 
-}
+// }
 
-exports.searchByNameGen = (value, search) => {
-    let newSearch = {};
-    if(value == ""){
-        return {};
-    }else if(search == "Name"){
-        newSearch = {"filter":{"searchFieldName":{"contains":value.toLowerCase()}}};
-        return newSearch;
-    }else {
-        newSearch = {"filter":{"searchFieldLocation":{"contains":value.toLowerCase()}}};
-        return newSearch;
-    }
-}
+// exports.searchByNameGen = (value, search) => {
+//     let newSearch = {};
+//     if(value == ""){
+//         return {};
+//     }else if(search == "Name"){
+//         newSearch = {"filter":{"searchFieldName":{"contains":value.toLowerCase()}}};
+//         return newSearch;
+//     }else {
+//         newSearch = {"filter":{"searchFieldLocation":{"contains":value.toLowerCase()}}};
+//         return newSearch;
+//     }
+// }
 
-exports.resetGen = () => {
-    return {};
-}
+// exports.resetGen = () => {
+//     return {};
+// }
