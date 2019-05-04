003760dfcacef6d5fc8b98c912b81c88e2c07010
Siddharth Rajan <siddharthrajan10@gmail.com>
2019-05-04 12:23:18 -0400

    begin modifying look of general info page

--- src/components/user_profile/content.js
+++ src/components/user_profile/content.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Tabs, Table, Button, Progress } from 'antd';
+import { Tabs, Table, Button, Progress, Card, Icon } from 'antd';
 import { getUser } from '../../services/auth';
 import { I18n } from 'aws-amplify';
 import AppliedJob from "./appliedJob";
@@ -21,8 +21,8 @@ const Information = (props) => {
             <Tabs defaultActiveKey="1" onChange={callback}>
                 <TabPane tab={I18n.get("General Info")} key="1">
                     <Progress type="circle" percent={75} format={percent => `${percent} Completed`} />
-                    <div>
-                        {I18n.get("First Name")}: {user.name}
+                    <div align="center">
+                        <h1>{I18n.get("Full Name")}: {user.name + " " + user.lastName}</h1>
                         <br />
                         {I18n.get("Last Name")}: {user.lastName}
                         <br />
@@ -38,7 +38,19 @@ const Information = (props) => {
                         <br />
                         {I18n.get("Spoken Language")}: {user.language}
                         <br />
+                        <Card
+                            size="default"
+                            title="User Information"
+                            style={{ width: "80%" }}
 
+                        >
+                            <p className="description" align="left" style={{ fontSize: 18 }}><Icon type="book" /><b> {I18n.get('Full Name')}: </b> {user.name + " " + user.middleName + " " + user.lastName}</p>
+                            <p className="description" align="left" style={{ fontSize: 18 }}><Icon type="home" /><b> {I18n.get('Age')}: </b>{user.age}</p>
+                            <p className="description" align="left" style={{ fontSize: 18 }}><Icon type="home" /><b> {I18n.get('English Level')}: </b>{user.englishLevel}</p>
+                            <p className="description" align="left" style={{ fontSize: 18 }}><Icon type="home" /><b> {I18n.get('Spoken Language')}: </b>{user.language}</p>
+                            <p className="description" align="left" style={{ fontSize: 18 }}><Icon type="home" /><b> {I18n.get('Email')}: </b>{user.email}</p>
+                            <p className="description" align="left" style={{ fontSize: 18 }}><Icon type="home" /><b> {I18n.get('Phone')}: </b>{user.phone}</p>
+                        </Card>
                     </div>
 
                 </TabPane>
