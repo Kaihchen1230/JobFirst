c99f6198ed30ab325e53f08b1aef83f06c0627d9
jieJayLan <lanjie3458@gmail.com>
2019-05-04 22:44:21 -0400

    add one dom test

--- src/components/business_profile/unitTest.test.js
+++ src/components/business_profile/unitTest.test.js
@@ -1,29 +1,24 @@
-import {render, fireEvent, wait} from 'react-testing-library';
+import {render, cleanup} from 'react-testing-library';
 import 'react-testing-library/cleanup-after-each';
 import React from 'react';
 import 'jest-dom/extend-expect';
 import BriefInfo from './briefInfo';
 
-const todos = [
-  {
-    title: 'todo1'
-  },
-  {
-    title: 'todo2'
-  }];
-
-describe('Todos', () => {
-  it('finds title', () => {
+describe('businessprofile', () => {
+  it('businessprofile - BriefInfo ', () => {
     const {getByText, getByTestId, container} 
         = render(
-            <BriefInfo companyType ="internet"
-                       headquarter ="NY"
-                       size = {2}
-                       companyWebsite = "alibaba.com"
-                       jobAmount ={3}
+            <BriefInfo companyType ={"internet"}
+                headquarter ="NY"
+                size = {2}
+                companyWebsite = "alibaba.com"
+                jobAmount ={3}
             />);
-    const elem = getByTestId('item');
-    expect(elem.innerHTML).toBe('todo1');
-
+    //if you want to use getByText, your text must be wrapped in a tap
+    expect(getByText('Brief Info')).toBeInTheDocument();
+    expect(getByText('internet')).toBeInTheDocument();
+    expect(getByText('NY')).toBeInTheDocument();
   })
 });
+
+cleanup();
