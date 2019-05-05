97a30d8d0993e86b018255df127680672f4af508
jieJayLan <lanjie3458@gmail.com>
2019-05-05 11:42:06 -0400

    finish the doc page for the business profile

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,4 +1,8 @@
 export const imports = {
+  'docs/businessProfile.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "docs-business-profile" */ 'docs/businessProfile.mdx'
+    ),
   'docs/index.mdx': () =>
     import(
       /* webpackPrefetch: true, webpackChunkName: "docs-index" */ 'docs/index.mdx'
