7a5b2ef3dc0b1dcd27cdfc093972f282a2c00219
Siddharth Rajan <siddharthrajan10@gmail.com>
2019-05-07 19:45:59 -0400

    add 2 input fields

--- src/components/form/addExperience.js
+++ src/components/form/addExperience.js
@@ -16,12 +16,32 @@ class AddExpForm extends React.Component {
         //this.typeUpdate = this.typeUpdate.bind(this);
     }
 
+    handleSubmit = async () => {
+
+    }
+
     render() {
         return (
             <div align="center">
                 <br />
-                <Form style={{ "width": "50%" }} name="experiencePost">
-                    
+                <h1>{I18n.get('Add an Experience')}</h1>
+                <Form onSubmit={this.handleSubmit} style={{ "width": "50%" }} name="experiencePost">
+                    <Form.Item>
+                        <Input placeholder={I18n.get('Enter the Company Name')}
+                            name="companyName"
+                            suffix={
+                                <Tooltip title={I18n.get('Enter the name of the company')}>
+                                    <Icon type="info-circle" />
+                                </Tooltip>}
+                        />
+                        <Input placeholder={I18n.get('Enter the City of the Company')}
+                            name="companyCity"
+                            suffix={
+                                <Tooltip title={I18n.get('Enter the city of the company')}>
+                                    <Icon type="info-circle" />
+                                </Tooltip>}
+                        />
+                    </Form.Item>
                 </Form>
             </div>
         )
