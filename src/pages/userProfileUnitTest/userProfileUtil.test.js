81429451e51bf26fc247b2730fc4bf81036fd563
Siddharth Rajan <siddharthrajan10@gmail.com>
2019-05-06 22:21:28 -0400

    add 2 unit tests

--- src/pages/userProfileUnitTest/userProfileUtil.test.js
+++ src/pages/userProfileUnitTest/userProfileUtil.test.js
@@ -15,3 +15,19 @@ test('should check for degree key', () => {
     const obj = checkKeyDegree(testObj);
     expect(obj).toEqual(true);
 });
+
+test('should check for city key', () => {
+    const testObj = {
+        city: '12345',
+    }
+    const obj = checkKeyCity(testObj);
+    expect(obj).toEqual(true);
+});
+
+test('should check for country key', () => {
+    const testObj = {
+        country: '12345',
+    }
+    const obj = checkKeyCountry(testObj);
+    expect(obj).toEqual(true);
+});
