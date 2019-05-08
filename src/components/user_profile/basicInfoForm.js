150a4875d605ba4152dc95b4c785b35e049c7b50
Alan Lau <alanlau97@gmail.com>
2019-05-08 17:23:31 -0400

    modified basic info form

--- src/components/user_profile/basicInfoForm.js
+++ src/components/user_profile/basicInfoForm.js
@@ -3,14 +3,61 @@ import {
     Button, Modal, Form, Input, Radio, Select,
 } from 'antd';
 
+let id = 0;
+
 const CollectionCreateForm = Form.create({ name: 'form_in_modal' })(
     // eslint-disable-next-line
     class extends React.Component {
         render() {
             const {
-                visible, onCancel, onCreate, form,
+                visible, onCancel, onCreate, form, add, remove,
             } = this.props;
-            const { getFieldDecorator } = form;
+            const { getFieldDecorator, getFieldValue } = form;
+            const formItemLayout = {
+                labelCol: {
+                    xs: { span: 24 },
+                    sm: { span: 4 },
+                },
+                wrapperCol: {
+                    xs: { span: 24 },
+                    sm: { span: 20 },
+                },
+            };
+            const formItemLayoutWithOutLabel = {
+                wrapperCol: {
+                    xs: { span: 24, offset: 0 },
+                    sm: { span: 20, offset: 4 },
+                },
+            };
+            getFieldDecorator('keys', { initialValue: [] });
+            const keys = getFieldValue('keys');
+            const formItems = keys.map((k, index) => (
+                <Form.Item
+                    {...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel)}
+                    label={index === 0 ? 'Passengers' : ''}
+                    required={false}
+                    key={k}
+                >
+                    {getFieldDecorator(`names[${k}]`, {
+                        validateTrigger: ['onChange', 'onBlur'],
+                        rules: [{
+                            required: true,
+                            whitespace: true,
+                            message: "Please input passenger's name or delete this field.",
+                        }],
+                    })(
+                        <Input placeholder="passenger name" style={{ width: '60%', marginRight: 8 }} />
+                    )}
+                    {keys.length > 1 ? (
+                        <Icon
+                            className="dynamic-delete-button"
+                            type="minus-circle-o"
+                            onClick={() => this.remove(k)}
+                        />
+                    ) : null}
+                </Form.Item>
+            ));
+            
             return (
                 <Modal
                     visible={visible}
@@ -39,8 +86,8 @@ const CollectionCreateForm = Form.create({ name: 'form_in_modal' })(
                         </Form.Item>
 
                         <Form.Item label="Age">
-                            {getFieldDecorator('age')(
-                                <Input />
+                            {getFieldDecorator('age', { initialValue: 15 })(
+                                <InputNumber min={15} max={100} />
                             )}
                         </Form.Item>
 
@@ -88,6 +135,44 @@ class BasicInfoForm extends React.Component {
         visible: false,
     };
 
+    remove = (k) => {
+        const { form } = this.formRef.props;
+        // can use data-binding to get
+        const keys = form.getFieldValue('keys');
+        // We need at least one passenger
+        if (keys.length === 1) {
+            return;
+        }
+
+        // can use data-binding to set
+        form.setFieldsValue({
+            keys: keys.filter(key => key !== k),
+        });
+    }
+
+    add = () => {
+        const { form } = this.formRef.props;
+        // can use data-binding to get
+        const keys = form.getFieldValue('keys');
+        const nextKeys = keys.concat(id++);
+        // can use data-binding to set
+        // important! notify form to detect changes
+        form.setFieldsValue({
+            keys: nextKeys,
+        });
+    }
+
+    // handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     this.props.form.validateFields((err, values) => {
+    //         if (!err) {
+    //             const { keys, names } = values;
+    //             console.log('Received values of form: ', values);
+    //             console.log('Merged values:', keys.map(key => names[key]));
+    //         }
+    //     });
+    // }
+
     showModal = () => {
         this.setState({ visible: true });
     }
@@ -122,6 +207,8 @@ class BasicInfoForm extends React.Component {
                     visible={this.state.visible}
                     onCancel={this.handleCancel}
                     onCreate={this.handleCreate}
+                    add={this.add}
+                    remove={this.remove}
                 />
             </div>
         );
