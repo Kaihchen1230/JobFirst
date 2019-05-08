e941fa9b67ffcecc139e8ab49594f259ad79a1b2
Siddharth Rajan <siddharthrajan10@gmail.com>
2019-05-07 20:46:06 -0400

    add all input fields for education

--- src/components/form/addEducation.js
+++ src/components/form/addEducation.js
@@ -16,7 +16,60 @@ class AddEduForm extends React.Component {
         console.log("The add education form loaded");
     }
 
+    handleSubmit = async () => {
+        <div align="cente">
+            <br />
+            <h1>{I18n.get('Add Education')}</h1>
+            <Form onSubmit={this.handleSubmit} style={{ "width": "50%" }} name="experiencePost">
+                <Form.Item>
+                    <Input placeholder={I18n.get('Enter the School Name')}
+                        name="schoolName"
+                        suffix={
+                            <Tooltip title={I18n.get('Enter the School Name')}>
+                                <Icon type="info-circle" />
+                            </Tooltip>}
+                    />
+                    <Input placeholder={I18n.get('Enter the Name of the Degree')}
+                        name="degreeName"
+                        suffix={
+                            <Tooltip title={I18n.get('Enter the Name of the Degree')}>
+                                <Icon type="info-circle" />
+                            </Tooltip>}
+                    />
+                    <Input placeholder={I18n.get('Enter the City of the School')}
+                        name="schoolCity"
+                        suffix={
+                            <Tooltip title={I18n.get('Enter the City of the School')}>
+                                <Icon type="info-circle" />
+                            </Tooltip>}
+                    />
+                    <Input placeholder={I18n.get('Enter the Country of the School')}
+                        name="schoolCountry"
+                        suffix={
+                            <Tooltip title={I18n.get('Enter the Country of the School')}>
+                                <Icon type="info-circle" />
+                            </Tooltip>}
+                    />
+                    <Input placeholder={I18n.get('Enter the Starting Year')}
+                        name="yearStart"
+                        suffix={
+                            <Tooltip title={I18n.get('Enter the Starting Year')}>
+                                <Icon type="info-circle" />
+                            </Tooltip>}
+                    />
+                    <Input placeholder={I18n.get('Enter the Ending Year')}
+                        name="yearEnd"
+                        suffix={
+                            <Tooltip title={I18n.get('Enter the Ending Year')}>
+                                <Icon type="info-circle" />
+                            </Tooltip>}
+                    />
+                </Form.Item>
+            </Form>
+        </div>
+    }
+
     render() {
-        
+
     }
 }
