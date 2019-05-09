ab6cc482c2dd304c3c2a923f6643d44d508e2205
Kaihchen1230 <kaihang.c@gmail.com>
2019-05-09 02:21:40 -0400

    completed 4 end2end tests

--- src/test/systemTest/employeeCheckResult.test.js
+++ src/test/systemTest/employeeCheckResult.test.js
@@ -1,9 +1,7 @@
 const puppeteer = require('puppeteer');
 const VIEWPORT = { width: 1300, height: 1000 };
 jest.setTimeout(70000);
-
-
-test('should able to login as an Employeer and accept the applicant', async () => {
+test('should able to login as an Employee and check the result of the application', async () => {
     const browser = await puppeteer.launch({
         headless: false
     });
@@ -16,24 +14,16 @@ test('should able to login as an Employeer and accept the applicant', async () =
     await page.click('li:nth-child(10)');
     await page.waitFor(1000);
     await page.click('#form_in_modal_userName');
-    await page.type('#form_in_modal_userName', 'kapp4');
+    await page.type('#form_in_modal_userName', 'kappa');
     await page.waitFor(1000);
     await page.click('#form_in_modal_password');
     await page.type('#form_in_modal_password', '214890303Khc');
     await page.waitFor(1000);
     await page.click('.ant-modal-footer > div > button:nth-child(2)');
     await page.waitFor(2000);
-    await page.click('li:nth-child(10)');
+    await page.click('li:nth-child(8)');
     await page.waitFor(2000);
-    await page.click('div[class*="ant-tabs-nav ant-tabs-nav-animated"] > div > div:last-child');
-    await page.waitFor(2000);
-    await page.click('main > div > div > div:last-child > div > div:last-child > div:last-child > button');
-    await page.waitFor(3000);
     await page.click('div[class*="ant-tabs-nav-wrap"] > div > div > div > div:last-child');
-    await page.waitFor(3000);
-    await page.click('tr > td:last-child > span > a:first-child');
-    await page.waitFor(2000);
-    await page.click('div[class*="ant-modal-footer"] > div > button:last-child');
     await page.waitFor(5000);
     browser.close();
 });
