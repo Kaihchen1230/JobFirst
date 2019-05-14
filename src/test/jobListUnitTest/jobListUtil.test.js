fd497178914b0ecd9a300a0fa09c080a905a3720
albert6051 <albert.chen6051@gmail.com>
2019-05-14 02:58:22 -0400

    new test for job list page added

--- src/test/jobListUnitTest/jobListUtil.test.js
+++ src/test/jobListUnitTest/jobListUtil.test.js
@@ -1,30 +1,61 @@
-const { filterTypeGen, filterDateGen, searchByNameGen, resetGen, } = require('./jobListUtil');
+const { filterTypeGen, filterDateGen, searchByNameGen, resetGen, filterCateGen, filterEduGen } = require('./jobListUtil');
 
 test('should give object for filter by job type', () => {
-    const obj = filterTypeGen('full time');
+    const obj = filterTypeGen('full time', { clickedCounts: { ge: 0 } });
     expect(obj["jobType"]).toEqual({ "contains": 'full time' });
 });
 
-// test('should give the date by minus current date to give value', () => {
-//     const obj = filterDateGen(10);
-//     let diff = 10 * 24 * 60 * 60 * 1000; //convert to milliseconds
-//     let current = new Date();
-//     let setTime = current - diff;
+test('sshould give an object contain the search field by category', () => {
+    const obj = filterCateGen('Healthcare', { clickedCounts: { ge: 0 } });
+    expect(obj["jobCategory"]).toEqual({ "contains": 'Healthcare' });
+})
 
-//     expect(obj["datePosted"]["ge"]).toEqual( setTime )
-// })
+test('sshould give an object contain the search field by education', () => {
+    const obj = filterEduGen('Bachelor', { clickedCounts: { ge: 0 } });
+    expect(obj["education"]).toEqual({ "contains": 'Bachelor' });
+})
 
-test('should give object for search by title', () => {
-    const obj = searchByNameGen('computer', 'Name');
-    expect(obj).toEqual({ "filter": { "searchFieldName": { "contains": 'computer' } } });
-});
+test('should give the correct value of date', () => {
+    const obj = filterDateGen(10, { clickedCounts: { ge: 0 } });
+    let diff = 10 * 24 * 60 * 60 * 1000; //convert to milliseconds
+    let current = new Date();
+    let setTime = current - diff;
+    let contorl = new Date(setTime);
+    let check = new Date(obj["datePosted"]["ge"]);
+    expect(check.getDate()+1).toEqual( contorl.getDate() )
+})
 
-test('should give object for search by location', () => {
-    const obj = searchByNameGen('180', 'Location');
-    expect(obj).toEqual({ "filter": { "searchFieldLocation": { "contains": '180' } } });
-});
+test('should give the correct value of month', () => {
+    const obj = filterDateGen(10, { clickedCounts: { ge: 0 } });
+    let diff = 10 * 24 * 60 * 60 * 1000; //convert to milliseconds
+    let current = new Date();
+    let setTime = current - diff;
+    let contorl = new Date();
+    let check = new Date(obj["datePosted"]["ge"]);
+    expect(check.getMonth()).toEqual( contorl.getMonth() )
+})
 
-test('should return an empty object', () => {
+test('should give the correct value of year', () => {
+    const obj = filterDateGen(10, { clickedCounts: { ge: 0 } });
+    let diff = 10 * 24 * 60 * 60 * 1000; //convert to milliseconds
+    let current = new Date();
+    let setTime = current - diff;
+    let contorl = new Date();
+    let check = new Date(obj["datePosted"]["ge"]);
+    expect(check.getFullYear()).toEqual( contorl.getFullYear() )
+})
+
+test('should give an object contain the search field by name', () => {
+    const obj = searchByNameGen('computer', 'Name', { clickedCounts: { ge: 0 } });
+    expect(obj["searchFieldName"]).toEqual({ "contains": 'computer' });
+})
+
+test('should give an object contain the search field by location', () => {
+    const obj = searchByNameGen('180', 'Location', { clickedCounts: { ge: 0 } });
+    expect(obj["searchFieldLocation"]).toEqual({ "contains": '180' });
+})
+
+test('should return an object contian click count filter field equal to 0', () => {
     const obj = resetGen();
-    expect(obj).toEqual({});
-});
+    expect(obj["clickedCounts"]["ge"]).toEqual(0);
+})
