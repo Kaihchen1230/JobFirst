9bff18d5c98288a7c29cb497a02b579fe48945c1
Kai Hang Chen <40642739+Kaihchen1230@users.noreply.github.com>
2019-05-24 22:35:19 -0400

    Update employerPostJob.test.js

--- src/test/systemTest/employerPostJob.test.js
+++ src/test/systemTest/employerPostJob.test.js
@@ -11,17 +11,17 @@ jest.setTimeout(70000);
         });
         const page = await browser.newPage();
         await page.goto(
-            'https://master.d311enjx0cy8vf.amplifyapp.com/'
+            'https://master.d1lcnidp5acn3o.amplifyapp.com'
         );
         await page.setViewport(VIEWPORT);
         await page.waitFor(1000);
         await page.click('li:nth-child(10)');
         await page.waitFor(1000);
         await page.click('#form_in_modal_userName');
-        await page.type('#form_in_modal_userName', 'kapp4');
+        await page.type('#form_in_modal_userName', 'employer');
         await page.waitFor(1000);
         await page.click('#form_in_modal_password');
-        await page.type('#form_in_modal_password', '214890303Khc');
+        await page.type('#form_in_modal_password', 'employer');
         await page.waitFor(1000);
         await page.click('.ant-modal-footer > div > button:nth-child(2)');
         await page.waitFor(2000);
